c6b184bdc56e4fee3b05615e455e5003ff632e43
luis <luis@aspire.zte.com.cn>
2021-05-12 00:24:27 -0500

    on linullx

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django import forms
-# Create your models here.
+# Create your models here
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from empresa.models import Empresa
