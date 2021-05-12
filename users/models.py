80eb5277564a34555cae49704d86eca1a1ae08dd
luis <luis@aspire.zte.com.cn>
2021-05-12 00:20:52 -0500

    on linux1

--- users/models.py
+++ users/models.py
@@ -9,7 +9,4 @@ from empresa.models import Empresa
 class MyUser(AbstractUser):
     empresa = models.ForeignKey(Empresa, null=True,related_name='MyUser',on_delete=models.CASCADE)
      #(queryset =Empresa.objects.all(), inicial = 0)
-    logo = models.ImageField(upload_to='uploads/',null=True, height_field=None, width_field=None, max_length=100)
-    logod = models.ImageField(upload_to='uploads/',null=True, height_field=None, width_field=None, max_length=100)
-    
-    
+    logo = models.ImageField(upload_to='uploads/',null=True, height_field=None, width_field=None, max_length=100)    
