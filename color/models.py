1e82878d3e7ebf95f71a3fcfdadd4543321599c3
Luis gabriel Liscano <ccidbcomputacion12@gmail.com>
2021-06-17 21:56:24 -0500

    prueba 85 cambio estilo de formularios Integrante

--- color/models.py
+++ color/models.py
@@ -5,8 +5,8 @@ from empresa.models import Empresa
 
 class Color(models.Model):
     usuario        = models.ForeignKey(User, related_name='Color', null=True, blank=True,on_delete=models.CASCADE)
-    empresa           = models.ForeignKey(Empresa, related_name='Color', null=False, blank=False,on_delete=models.CASCADE)
-    nom_color      = models.CharField(max_length=20, unique=True)
+    empresa        = models.ForeignKey(Empresa, related_name='Color', null=False, blank=False,on_delete=models.CASCADE)
+    nom_color      = models.CharField(max_length=20)
     codigo_color   = models.IntegerField()    
     created_at     = models.DateTimeField(auto_now_add=True)
     updated_at     = models.DateTimeField(auto_now=True)
