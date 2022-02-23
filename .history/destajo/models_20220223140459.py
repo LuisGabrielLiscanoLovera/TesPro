9e6a5918e07310a31cdc14b5fbadee9e71ea4b25
[gabriel <[ccidbcomputacion12@gmail.com]>
2022-02-23 14:26:26 -0500

    'automatico'

---
+++ .history/destajo/models_20220223140459.py
@@ -0,0 +1,21 @@
+from django.db import models
+from integrante.models import Integrante
+from tarea.models import Tarea
+from operacion.models import Operacion
+# Create your models here.
+from authapp.models import MyUser as User
+class Destajo(models.Model):
+    nom_operacion = models.ForeignKey(Operacion, related_name='Destajo',on_delete=models.CASCADE)
+    tarea         = models.ForeignKey(Tarea, related_name='Destajo',on_delete=models.CASCADE)
+    integrante    = models.ForeignKey(Integrante, related_name='Destajo',on_delete=models.CASCADE)
+    cantidad      = models.IntegerField(blank=True, null=True)
+    talla         = models.CharField(blank=True, null=True,max_length=5)
+    #can_resta     = models.IntegerField(blank=True, null=True)
+    # user = models.ForeignKey(User, related_name='destajo',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #user      = models.ForeignKey(User, related_name='Destajo', null=False, blank=False,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.integrante)
+    
