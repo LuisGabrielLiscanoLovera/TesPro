eddae54c11512a0aaf3635ff27a974e10193867c
[gabriel <[ccidbcomputacion12@gmail.com]>
2022-05-17 01:31:37 -0500

    'automatico'

---
+++ TesPro/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# define the definition/function to return our template page
+def zinggrid(request):
+    title = 'sMque paso!'
+    return render(request, 'zinggrid.html', {'title': title})
+
+
+def verjson(request):
+    data={
+    
+      'first_name': 'Airi',
+      'last_name': 'Satou',
+      'position': 'Accountant',
+      'office': 'Tokyo',
+      'start_date': '28th Nov 08',
+      'salary': '$162,700'
+    }
+  
+    return JsonResponse(data)
