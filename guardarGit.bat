53921e5961ddc75e70465f02b88b5c3bb77f5583
[gabriel <[ccidbcomputacion12@gmail.com]>
2022-06-02 16:44:35 -0500

    'automatico'

--- guardarGit.bat
+++ guardarGit.bat
@@ -10,4 +10,4 @@ for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
 set ip=%ip:~1%
 echo %ip%
 
-python .\manage.py runserver echo %ip%:4433
+python .\manage.py runserver %ip%:4433
