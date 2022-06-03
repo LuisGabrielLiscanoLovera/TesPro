6b5dd3a8d7f3ecb431f1d7354be2b709e16fc31e
[gabriel <[ccidbcomputacion12@gmail.com]>
2022-06-02 19:31:56 -0500

    se modifica commit

--- guardarGit.bat
+++ guardarGit.bat
@@ -1,17 +1,14 @@
 @echo off
 set /p CommitName="Commit: "
-echo Commi es : %CommitName%
- 
- python keyC.py
- git add .
- git status 
- git commit -m "%CommitName%"
- git push
- python keyE.py
- python keyD.py
+python keyC.py
+git add .
+git status 
+git commit -m "%CommitName%"
+git push
+python keyE.py
+python keyD.py
  
 for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
 set ip=%ip:~1%
 echo %ip%
-
 python .\manage.py runserver %ip%:4433
