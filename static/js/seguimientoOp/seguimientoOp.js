8186a0867c935f539dafc009d5f5136e07336210
[gabriel <[ccidbcomputacion12@gmail.com]>
2022-06-29 21:36:41 -0500

    seguimiento e tarea

--- static/js/seguimientoOp/seguimientoOp.js
+++ static/js/seguimientoOp/seguimientoOp.js
@@ -38,45 +38,17 @@ function DetailFormatterButInfoseguimientoOp(index, row) {
 
 
 
-        '<div class="col-sm-4 md-1 " style="overflow-x:auto;position:relative">' +
-        '<label>Integrantes en produccion</label>' +
-
-
-        '<table id="tableSintegrante-' + row.id + '" class=" thead-dark table fixed_header  ">' +
-        '<thead class="">' +
-        '<tr>' + '<th scope="col" class="text-center">Nombre</th>' +
-        '<th scope="col" class="text-center">Apellido</th>' +
-        '<th scope="col" class="text-center">Cedula</th>' +
-        '</tr>' +
-        '</thead>' +
-        '<tbody>' +
-        '<tr v-for="inte in integranteOPListSeguimiento">' +
-        '<td class="text-center">[[inte.nomIntegrante]]</td>' +
-        '<td class="text-center">[[inte.apeIntegrante]]</td>' +
-        '<td class="text-center">[[inte.cedulaIntegrante]]</td>' +
-        '</tr>' +
-        '</tbody>' +
-        '</table>' +
-        '</div>' +
-
-
-
-
-
-
-
-
 
 
         '<div class="col-sm-4 md-1 " style="overflow-x:auto;position:relative">' +
-        '<label>Patinador en produccion</label>' +
+        '<label>Trea en produccion</label>' +
         '<div class="tableContainer">' +
 
         '<table id="tableSpatinador-' + row.id + '" class="thead-dark table fixed_header ">' +
         '<thead class="">' +
-        '<tr>' + '<th scope="col" class="text-center">Nombreeee</th>' +
-        '<th scope="col" class="text-center">Apellido</th>' +
-        '<th scope="col" class="text-center">Cedula</th>' +
+        '<tr>' + '<th scope="col" class="text-center">Tarea</th>' +
+        '<th scope="col" class="text-center">Talla</th>' +
+        '<th scope="col" class="text-center">Cantidad</th>' +
         '</tr>' +
         '</thead>' +
         '<tbody>' +
@@ -99,6 +71,58 @@ function DetailFormatterButInfoseguimientoOp(index, row) {
         '</div>' +
         '</div>';
 }
+
+
+
+
+
+/*
+
+        '<div class="col-sm-4 md-1 " style="overflow-x:auto;position:relative">' +
+        '<label>Integrantes en produccion</label>' +
+
+
+        '<table id="tableSintegrante-' + row.id + '" class=" thead-dark table fixed_header  ">' +
+        '<thead class="">' +
+        '<tr>' + '<th scope="col" class="text-center">Nombre</th>' +
+        '<th scope="col" class="text-center">Apellido</th>' +
+        '<th scope="col" class="text-center">Cedula</th>' +
+        '</tr>' +
+        '</thead>' +
+        '<tbody>' +
+        '<tr v-for="inte in integranteOPListSeguimiento">' +
+        '<td class="text-center">[[inte.nomIntegrante]]</td>' +
+        '<td class="text-center">[[inte.apeIntegrante]]</td>' +
+        '<td class="text-center">[[inte.cedulaIntegrante]]</td>' +
+        '</tr>' +
+        '</tbody>' +
+        '</table>' +
+        '</div>' +
+
+                '<div class="col-sm-4 md-1 " style="overflow-x:auto;position:relative">' +
+                '<label>Patinador en produccion</label>' +
+                '<div class="tableContainer">' +
+
+                '<table id="tableSpatinador-' + row.id + '" class="thead-dark table fixed_header ">' +
+                '<thead class="">' +
+                '<tr>' + '<th scope="col" class="text-center">Nombre</th>' +
+                '<th scope="col" class="text-center">Apellido</th>' +
+                '<th scope="col" class="text-center">Cedula</th>' +
+                '</tr>' +
+                '</thead>' +
+                '<tbody>' +
+                '<tr v-for="inteP in patinadorOPListSeguimiento">' +
+                '<td class="text-center">[[inteP.nomPatinador]]</td>' +
+                '<td class="text-center">[[inteP.apellPatinador]]</td>' +
+                '<td class="text-center">[[inteP.cedulaPatinador]]</td>' +
+                '</tr>' +
+                '</tbody>' +
+                '</table>' +
+                '</div>' +
+                '</div>' +
+*/
+
+
 
 
 function SeguimientoOp(idOperacion, idUsuario) {
